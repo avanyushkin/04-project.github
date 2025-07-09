5782c0e758c93990de63f4e33c89304a09c93004
avanyushkin <a.vanyushkin1@gmail.com>
2025-07-09 08:31:37 +0300

    Update script.js
    update touch-actions

--- script.js
+++ script.js
@@ -4,39 +4,44 @@ const cnt = 500;
 for (let i = 0; i < cnt; i++) {
     const square = document.createElement('div'); // динамически создаем элемент
     square.classList.add('square');
-
-    square.addEventListener('mouseover', () => {
-        //console.log('over');
-        setColor(square);
-    });
-
-    square.addEventListener('mouseleave', () => {
-        removeColor(square);
-    })
-
-    // добавим события для мобилки
-    square.addEventListener('touchstart', () => {
-        setColor(square);
-    })
-    square.addEventListener('touchmove', (x) => {
-        x.preventDefault(); // при передвижании пальца
-        const touch = x.touches[0];
-        const element = document.elementFromPoint(touch.clientX, touch.clientY);
-        if (element && element.classList.contains('square')) {
-            setColor(square);
-        }
-    })
-    square.addEventListener('touchend', () => {
-        removeColor(square);
-    })
-
     board.append(square); // добавляем элемент в html
 }
 
+// обрабатываем события для мыши
+board.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('square')) {
+        setColor(event);
+    }
+});
+board.addEventListener('mouseout', (event) => {
+    if (event.target.classList.contains('square')) {
+        removeColor(event);
+    }
+});
+
+// обрабатываем события для мобилки
+board.addEventListener('touchmove', (event) => {
+    event.preventDefault();
+    const touches = event.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+        const touch = touches[i];
+        const element = document.elementFromPoint(touch.clientX, touch.clientY);
+
+        if (element && element.classList.contains('square')) {
+            element.style.background = 'red';
+        }
+    }
+});
+board.addEventListener('touchend', (event) => { // для сбрасывания кваратов
+    document.querySelectorAll('.square').forEach(square => {
+        square.style.backgroundColor = '#1d1d1d';
+    });
+});
+
 function setColor(x) {
-    x.style.backgroundColor = "red";
+    x.target.style.backgroundColor = "red";
 }
 
 function removeColor(x) {
-    x.style.backgroundColor = '#1d1d1d';
+    x.target.style.backgroundColor = '#1d1d1d';
 }
